9072ddb403d90897639ca3f30c2306a19f434148
dkolarova <dkolarova@CSCBGRAH753702.obs.local>
2017-10-16 16:54:22 +0300

    Javadoc

--- toMockOrToStub/src/main/java/edu/design/mockorstub/services/external/ExternalDataService.java
+++ toMockOrToStub/src/main/java/edu/design/mockorstub/services/external/ExternalDataService.java
@@ -3,8 +3,20 @@ package edu.design.mockorstub.services.external;
 import java.util.Collection;
 import java.util.List;
 
+/**
+ * Loads data from a remote data repository.
+ */
 public interface ExternalDataService {
-	
+
+	/**
+	 * Loads data from a remote repository based on a list of identifiers and
+	 * returns the data serialized into text values.
+	 * 
+	 * @param ids
+	 *            A list of data identifiers
+	 * 
+	 * @return List of objects represented as textual values.
+	 */
 	List<String> loadData(Collection<String> ids);
 
 }
--- toMockOrToStub/src/main/java/edu/design/mockorstub/services/internal/InternalDataService.java
+++ toMockOrToStub/src/main/java/edu/design/mockorstub/services/internal/InternalDataService.java
@@ -2,8 +2,19 @@ package edu.design.mockorstub.services.internal;
 
 import java.util.List;
 
+/**
+ * Loads data stored into application's domain objects.
+ */
 public interface InternalDataService {
-	
+
+	/**
+	 * Loads a list of domain objects based an a list of identifiers passed.
+	 * 
+	 * @param ids
+	 *            List of object identifiers to be serached for
+	 * @return List of domain objects corresponding to the identifiers passed for
+	 *         search
+	 */
 	List<DomainObject> loadDomainObjects(List<String> ids);
 
 }
